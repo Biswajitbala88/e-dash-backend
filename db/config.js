4cae57b419631de550fe03299ba2e10b50216439
Biswajitbala88 <biswajitbala88@gmail.com>
2023-12-17 17:08:13 +0530

    added some comment 2

--- db/config.js
+++ db/config.js
@@ -2,3 +2,5 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/e-dash');
 
 // In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available. 
+
+// In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available. 

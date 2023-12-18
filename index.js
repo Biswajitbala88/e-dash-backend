aa126a9d2b1994977c67e4740d08db4830f31f04
Biswajitbala88 <biswajitbala88@gmail.com>
2023-12-18 23:32:10 +0530

    added signin and signup apis

--- index.js
+++ index.js
@@ -1,10 +1,37 @@
 const express = require('express');
+require('./db/config');
+const cors = require('cors');
 const app = express();
+const User = require('./db/User');
+app.use(express.json());
+app.use(cors());
 
+// sign up
+app.post("/signup", async (req, resp)=>{
+    const user = new User(req.body);
+    let result = await user.save();
+    result = result.toObject();
+    delete result.password;
+    resp.send(result);
+});
 
-
-app.post("/signup", (req, resp)=>{
-    resp.send("working fine");
+// sign in
+app.post("/signin", async (req, resp)=>{
+    if(req.body.email){
+        if(req.body.password){
+            const user = await User.findOne(req.body).select('-password');
+            if(user){
+                resp.send(user);
+            }else{
+                resp.send({result: "No user found"});
+            }
+        }else{
+            resp.send({result: "please enter password"});
+        }
+    }else{
+        resp.send({result: "please enter email"});
+    }
+    
 });
 
 app.listen(1200);
